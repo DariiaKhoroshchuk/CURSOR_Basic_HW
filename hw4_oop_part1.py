1bcf985799c6a9a895d23ec0f4247f505a46e353
Dariia Khoroshchuk <dkhoroshchuk@gmail.com>
2021-03-10 00:05:43 +0200

    adding finished hw4

--- hw4_oop_part1.py
+++ hw4_oop_part1.py
@@ -7,7 +7,8 @@ class Vehicle:
         self.mileage = mileage
 
 
-# 2. Create a child class Bus that will inherit all of the variables and methods of the Vehicle class and will have seating_capacity own method
+# 2. Create a child class Bus that will inherit all of the variables and methods of the Vehicle class and will have
+# seating_capacity own method
 
 class Bus(Vehicle):
     def __init__(self, max_speed, mileage, seating_capacity):
@@ -52,7 +53,8 @@ school.school_id()
 # output:
 # School id is 12345.
 
-# 6*. Create a new class SchoolBus that will inherit all of the methods from School and Bus and will have its own - bus_school_color
+# 6*. Create a new class SchoolBus that will inherit all of the methods from School and Bus and will have
+# its own - bus_school_color
 
 
 class SchoolBus(School, Bus):
@@ -62,23 +64,106 @@ class SchoolBus(School, Bus):
         self.bus_school_color = bus_school_color
 
 
-# 7. Polymorphism: Create two classes: Bear, Wolf. Both of them should have make_sound method. Create two instances, one of Bear and one of Wolf,
+# 7. Polymorphism: Create two classes: Bear, Wolf. Both of them should have make_sound method. Create two instances,
+# one of Bear and one of Wolf,
 # make a tuple of it and by using for call their action using the same method.
+
+class Bear:
+    def make_sound(self):
+        print('Argh')
+
+
+class Wolf:
+    def make_sound(self):
+        print('Awoo')
+
+
+bear = Bear()
+wolf = Wolf()
+
+for animals in (bear, wolf):
+    animals.make_sound()
 
 
 # Magic methods:
 # 8. Create class City with name, population instance attributes, return a new instance only when population > 1500,
 # otherwise return message: "Your city is too small".
 
-# 9. Override a printable string representation of the City class and return: The population of the city {name} is {population}
+class City:
+    def __init__(self, name, population):
+        self.name = name
+        self.population = population
+
+    def __new__(cls, name, population):
+        print(f'Creating the city {name}')
+        city = super(City, cls).__new__(cls)
+        if population > 1500:
+            return city
+        else:
+            return 'Your city is too small'
+
+# 9. Override a printable string representation of the City class and return:
+# The population of the city {name} is {population}
+
+    def __str__(self):
+        return f'The population of the city {self.name} is {self.population}'
 
 
-# 10*. Override magic method __add__() to perform the additional action as 'multiply' (*) the value which is greater than 10. And perform this add (+) of two instances.
+city_1 = City('Big_city', 700000)
+print(city_1)
+city_2 = City('Small_city', 1300)
+print(city_2)
+
+# output:
+# Creating the city Big_city
+# The population of the city Big_city is 700000
+# Creating the city Small_city
+# Your city is too small
+
+# 10*. Override magic method __add__() to perform the additional action as 'multiply' (*) the value which is
+# greater than 10. And perform this add (+) of two instances.
 
 
-# 11. The __call__ method enables Python programmers to write classes where the instances behave like functions and can be called like a function.
+class SumNum:
+    def __init__(self, num):
+        self.num = num
+
+    def __add__(self, other):
+        if self.num > 10:
+            return self.num * other.num
+        else:
+            return self.num + other.num
+
+
+num_1 = SumNum(3)
+num_2 = SumNum(25)
+
+print(num_1 + num_2)
+print(num_2 + num_1)
+
+# output:
+# 28
+# 75
+
+# 11. The __call__ method enables Python programmers to write classes where the instances behave like functions and
+# can be called like a function.
 # Create a new class with __call__ method and define this call to return sum.
 
+
+class CallMethod:
+    def __call__(self, *args):
+        return sum(args)
+
+
+method_1 = CallMethod()
+print(method_1(5, 10))
+print(method_1(5, 10, 15))
+print(method_1(5, 10, -15, -20))
+
+# output:
+# 15
+# 30
+# -20
 
 # 12*. Making Your Objects Truthy or Falsey Using __bool__().
 # Create class MyOrder with cart and customer instance attributes.
@@ -89,4 +174,25 @@ class SchoolBus(School, Bus):
 # bool(order_1)
 # True
 # bool(order_2)
+# False
+
+
+class MyOrder:
+    def __init__(self, card, customer):
+        self.card = card
+        self.customer = customer
+
+    def __bool__(self):
+        return True if len(self.card) != 0 else False
+
+
+order_1 = MyOrder(['a', 'b', 'c'], 'd')
+order_2 = MyOrder([], 'a')
+
+print(bool(order_1))
+print(bool(order_2))
+
+
+# output:
+# True
 # False
