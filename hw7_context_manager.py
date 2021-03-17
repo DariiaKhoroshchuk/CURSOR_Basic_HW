1a2490d73fb1d3e44500ec735a767ef82f27c3a2
Dariia Khoroshchuk <dkhoroshchuk@gmail.com>
2021-03-17 16:57:44 +0200

    adding not finished hw7

---
+++ hw7_context_manager.py
@@ -0,0 +1,48 @@
+import pickle
+# import openpyxl
+# 1
+with open('task1.txt', 'r') as file:
+    dict_sub = {}
+    subtitles = file.readlines()
+    for line in range(0, len(subtitles), 2):
+        subtitles[line] = subtitles[line].replace('\n', '')
+        subtitles[line+1] = subtitles[line+1].replace('\n', '')
+        dict_sub.update({subtitles[line]: subtitles[line+1]})
+for key, val in dict_sub.items():
+    print(f'{key} : {val}')
+
+# output:
+# 00:00 : We all know you should take a sick day
+# 00:01 : or a mental health day
+# 00:03 : or even a personal day.
+# 00:04 : But a financial day is just as important.
+# ...
+
+with open('task1_sub.txt', 'w') as file:
+    for line in dict_sub.values():
+        file.write(f'{line} ')
+
+# file content:
+# We all know you should take a sick day or a mental health day or even a personal day. But a financial day is just...
+
+# 2
+quantity = 0
+total_sum = 0
+with open('task2', 'rb') as file2:
+    numbers = file2.read()
+    numbers_b = pickle.loads(numbers)
+print(f'List of numbers from the file: {numbers_b}.')
+for i in numbers_b:
+    total_sum += i
+    quantity += 1
+print(f'The sum of the file numbers is equal to {total_sum}.')
+arithmetic_mean = total_sum / quantity
+print(f'The arithmetic mean of the file numbers is equal to {arithmetic_mean}.')
+
+# output:
+# List of numbers from the file: [1, 1, 1, 1, 1, 1, 1, 2, 11, 11, 3123, 34, 12, 2, 324, 9, 9, 9, 10].
+# The sum of the file numbers is equal to 3563.
+# The arithmetic mean of the file numbers is equal to 187.52631578947367.
+
+
+
